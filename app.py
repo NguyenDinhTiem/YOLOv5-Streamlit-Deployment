623a3c2314193b03a30702e53e6e7a58b059a606
NguyenDinhTiem <59758186+NguyenDinhTiem@users.noreply.github.com>
2023-01-27 03:26:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def videoInput(device, src):
             st.download_button('Download output', f, file_name= uploaded_video.name)  # Defaults to 'application/octet-stream'
 
 def load_model(cfg_model_path):
-    return torch.hub.load('ultralytics/yolov5', 'custom', path = cfg_model_path , force_reload=True, _verbose=False) 
+    return torch.hub.load('ultralytics/yolov5', 'custom', path = cfg_model_path, _verbose=False) 
 
 
 def main():
